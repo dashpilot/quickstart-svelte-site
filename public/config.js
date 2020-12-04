fc3896a732f18a0fd4d1fc04eb56b43552c1a459
dashpilot <suport@dashpilot.com>
2020-12-04 23:33:56 +0100

    Moved config to config.js

---
+++ public/config.js
@@ -0,0 +1,7 @@
+config = {};
+config.basePath = 'https://editscreen-shared-data.vercel.app/';
+config.dataPath = 'data.json';
+config.buttons = ['add'];
+config.layouts = ['post'];
+config.assetsPath = '/assets/';
+config.page = 'home';
